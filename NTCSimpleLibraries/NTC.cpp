bdbaa1a44a8e9b7ef7bc8707cd66fce6161e4f08
Davide <davidebrag@gmail.com>
2020-05-26 20:21:35 +0200

    Add files via upload

---
+++ NTCSimpleLibraries/NTC.cpp
@@ -0,0 +1,48 @@
+//Bragatto Davide, 25/05/2020 20:03, libreriaNTC
+
+#include "Arduino.h"
+#include "NTC.h"
+
+void NTC::initialize(int a)
+{
+  pinNtc=a;
+}
+
+float NTC::tempCelsius(int b)
+{
+  tempReading=analogRead(pinNtc);
+  tempK = log(10000.0 * ((1023.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );
+  tempC = tempK - 273.15;
+  delay(b);
+  return tempC;
+}
+
+float NTC::tempFahrenheit(int c)
+{
+  tempReading=analogRead(pinNtc);
+  tempK = log(10000.0 * ((1023.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );
+  tempC = tempK - 273.15;
+  tempF = (tempC * 9.0)/ 5.0 + 32.0;
+  delay(c);
+  return tempF;
+}
+
+float NTC::tempKelvin(int d)
+{
+  tempReading=analogRead(pinNtc);
+  tempK = log(10000.0 * ((1023.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );
+  delay(d);
+  return tempK;
+}
+
+float NTC::valueVolts(int e)
+{
+  tempReading = analogRead(pinNtc);
+  tempVolts = tempReading * 5.0 / 1023.0;
+  delay(e);
+  return tempVolts;
+}
+
---
+++ NTCSimpleLibraries/NTC.h
@@ -0,0 +1,25 @@
+//Bragatto Davide, 25/05/2020 20:03, libreriaNTC
+
+#ifndef NTC_h
+#define NTC_h
+#include "Arduino.h"
+
+class NTC
+{
+  private:
+    int pinNtc;
+    int tempReading;
+    float tempK;
+    float tempC;
+    float tempF;
+    float tempVolts;
+  public:
+    void initialize(int a);
+    float tempCelsius(int b);
+    float tempFahrenheit(int c);
+    float tempKelvin(int d);
+    float valueVolts(int e);
+};
+
+#endif
+
